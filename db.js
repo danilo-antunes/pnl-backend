187d4afda569cf27ed6f1a46345add40d46da04d
danilo-antunes <danilo.fe.antunes@gmail.com>
2023-06-16 02:43:40 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "crud"
+    host: "us-cdbr-east-06.cleardb.net",
+    user: "b351e072f369f8",
+    password: "78ffca45",
+    database: "heroku_cd7c8c30010f56c"
 })
